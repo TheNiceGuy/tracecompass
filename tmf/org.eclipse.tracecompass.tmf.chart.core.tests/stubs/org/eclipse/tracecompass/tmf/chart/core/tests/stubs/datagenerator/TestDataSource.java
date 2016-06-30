b57c00eaf24def948fb5688f90789696ff106080
Gabriel-Andrew Pollo-Guilbert <gabrielpolloguilbert@gmail.com>
2016-06-30 17:28:28 -0400

    Add JUnit test skelons

---
+++ tmf/org.eclipse.tracecompass.tmf.chart.core.tests/stubs/org/eclipse/tracecompass/tmf/chart/core/tests/stubs/datagenerator/TestDataSource.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2016 École Polytechnique de Montréal
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.tracecompass.tmf.chart.core.tests.stubs.datagenerator;
+
+import org.eclipse.tracecompass.internal.tmf.chart.core.module.AbstractDataModel;
+
+public class TestDataSource extends AbstractDataModel {
+    public TestDataSource(String name) {
+        super(name);
+        // TODO Auto-generated constructor stub
+    }
+
+}
