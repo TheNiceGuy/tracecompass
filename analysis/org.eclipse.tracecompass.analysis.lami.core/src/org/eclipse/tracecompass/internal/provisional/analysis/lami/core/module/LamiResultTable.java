42e089f7612154bc78cfb750abe489e8a3e6d8c2
Gabriel-Andrew Pollo-Guilbert <gabrielpolloguilbert@gmail.com>
2016-07-07 15:11:20 -0400

    analysis.lami: Implement a data provider
    This patch adds a data model for a LAMI analysis. It
    also implements a data provider into the result table.
    This allows creation of custom charts using the
    upcoming plugin.
    
    Change-Id: I1375c81c6e99538d4b2e86afb517c4014b0a8a98
    Signed-off-by: Gabriel-Andrew Pollo-Guilbert <gabrielpolloguilbert@gmail.com>

--- analysis/org.eclipse.tracecompass.analysis.lami.core/src/org/eclipse/tracecompass/internal/provisional/analysis/lami/core/module/LamiResultTable.java
+++ analysis/org.eclipse.tracecompass.analysis.lami.core/src/org/eclipse/tracecompass/internal/provisional/analysis/lami/core/module/LamiResultTable.java
@@ -9,22 +9,50 @@
 
 package org.eclipse.tracecompass.internal.provisional.analysis.lami.core.module;
 
-import java.util.List;
+import static org.eclipse.tracecompass.common.core.NonNullUtils.checkNotNull;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.tracecompass.internal.provisional.analysis.lami.core.aspect.LamiEmptyAspect;
+import org.eclipse.tracecompass.internal.provisional.analysis.lami.core.aspect.LamiTableEntryAspect;
 import org.eclipse.tracecompass.internal.provisional.analysis.lami.core.types.LamiTimeRange;
+import org.eclipse.tracecompass.internal.provisional.tmf.chart.core.descriptor.DataChartDurationDescriptor;
+import org.eclipse.tracecompass.internal.provisional.tmf.chart.core.descriptor.DataChartNumericalDescriptor;
+import org.eclipse.tracecompass.internal.provisional.tmf.chart.core.descriptor.DataChartStringDescriptor;
+import org.eclipse.tracecompass.internal.provisional.tmf.chart.core.descriptor.DataChartTimestampDescriptor;
+import org.eclipse.tracecompass.internal.provisional.tmf.chart.core.descriptor.IDataChartDescriptor;
+import org.eclipse.tracecompass.internal.provisional.tmf.chart.core.model.IDataChartProvider;
+import org.eclipse.tracecompass.internal.provisional.tmf.chart.core.resolver.AbstractLongResolver;
+import org.eclipse.tracecompass.internal.provisional.tmf.chart.core.resolver.IStringResolver;
 
 import com.google.common.collect.ImmutableList;
 
 /**
- * Class holding the results contained in one table outputted by a LAMI analysis.
+ * Class holding the results contained in one table outputted by a LAMI
+ * analysis.
  *
  * @author Alexandre Montplaisir
+ * @author Gabriel-Andrew Pollo-Guilbert
  */
-public class LamiResultTable {
+public class LamiResultTable implements IDataChartProvider<LamiTableEntry> {
+
+    // ------------------------------------------------------------------------
+    // Members
+    // ------------------------------------------------------------------------
 
     private final LamiTimeRange fTimeRange;
     private final LamiTableClass fTableClass;
     private final List<LamiTableEntry> fEntries;
+    private final List<IDataChartDescriptor<LamiTableEntry, ?>> fDescriptors;
+
+    // ------------------------------------------------------------------------
+    // Constructors
+    // ------------------------------------------------------------------------
 
     /**
      * Construct a new table from its components.
@@ -42,7 +70,53 @@ public class LamiResultTable {
         fTimeRange = timeRange;
         fTableClass = tableClass;
         fEntries = ImmutableList.copyOf(entries);
+        fDescriptors = new ArrayList<>();
+
+        for (LamiTableEntryAspect aspect : getTableClass().getAspects()) {
+            if (aspect instanceof LamiEmptyAspect) {
+                continue;
+            }
+
+            if (aspect.isContinuous()) {
+                if (aspect.isTimeStamp()) {
+                    /* Create descriptors for timestamps */
+                    fDescriptors.add(new DataChartTimestampDescriptor<>(aspect.getName(), new LamiDataLongResolver(aspect)));
+                } else if (aspect.isTimeDuration()) {
+                    /* Create descriptors for time durations */
+                    fDescriptors.add(new DataChartDurationDescriptor<>(aspect.getName(), new LamiDataLongResolver(aspect)));
+                } else {
+                    /* Create descriptors for general numbers */
+                    fDescriptors.add(new DataChartNumericalDescriptor<>(aspect.getName(), new LamiDataLongResolver(aspect)));
+                }
+            } else {
+                /* Create descriptors for general strings */
+                fDescriptors.add(new DataChartStringDescriptor<>(aspect.getName(), new LamiDataStringResolver(aspect)));
+            }
+        }
     }
+
+    // ------------------------------------------------------------------------
+    // Overriden methods
+    // ------------------------------------------------------------------------
+
+    @Override
+    public String getName() {
+        return getTableClass().getTableTitle();
+    }
+
+    @Override
+    public Stream<@NonNull LamiTableEntry> getSource() {
+        return checkNotNull(fEntries.stream());
+    }
+
+    @Override
+    public List<IDataChartDescriptor<LamiTableEntry, ?>> getDataDescriptors() {
+        return fDescriptors;
+    }
+
+    // ------------------------------------------------------------------------
+    // Accessors
+    // ------------------------------------------------------------------------
 
     /**
      * Get the time range of this table.
@@ -70,4 +144,35 @@ public class LamiResultTable {
     public List<LamiTableEntry> getEntries() {
         return fEntries;
     }
+
+    // ------------------------------------------------------------------------
+    // Data resolvers
+    // ------------------------------------------------------------------------
+
+    private final class LamiDataLongResolver extends AbstractLongResolver<LamiTableEntry> {
+        LamiTableEntryAspect fAspect;
+
+        public LamiDataLongResolver(LamiTableEntryAspect aspect) {
+            fAspect = aspect;
+        }
+
+        @Override
+        public Function<LamiTableEntry, @Nullable Long> getMapper() {
+            return o -> checkNotNull(fAspect.resolveNumber(o)).longValue();
+        }
+    }
+
+    private final class LamiDataStringResolver implements IStringResolver<LamiTableEntry> {
+        LamiTableEntryAspect fAspect;
+
+        public LamiDataStringResolver(LamiTableEntryAspect aspect) {
+            fAspect = aspect;
+        }
+
+        @Override
+        public Function<@NonNull LamiTableEntry, @Nullable String> getMapper() {
+            return o -> fAspect.resolveString(o);
+        }
+    }
+
 }
