cbe0a6b0e1c0fca10f9d611210eda294554d333c
Gabriel-Andrew Pollo-Guilbert <gabrielpolloguilbert@gmail.com>
2016-07-11 12:19:43 -0400

    custom charts: Add factory constructor for charts
    This patch adds the factory constructor for making any
    type of charts.
    
    Change-Id: I9f82bc65c2d9b13ab52bf0494fd27e550d1e96a8
    Signed-off-by: Gabriel-Andrew Pollo-Guilbert <gabrielpolloguilbert@gmail.com>

---
+++ tmf/org.eclipse.tracecompass.tmf.chart.ui/src/org/eclipse/tracecompass/tmf/chart/ui/IChartViewer.java
@@ -0,0 +1,79 @@
+/*******************************************************************************
+ * Copyright (c) 2016 École Polytechnique de Montréal
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.tracecompass.tmf.chart.ui;
+
+import static org.eclipse.tracecompass.common.core.NonNullUtils.checkNotNull;
+
+import java.util.List;
+
+import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.tracecompass.tmf.chart.core.model.ChartData;
+import org.eclipse.tracecompass.tmf.chart.core.model.ChartModel;
+
+import com.google.common.collect.ImmutableList;
+
+/**
+ * Base class and factory constructor for charts.
+ *
+ * @author Gabriel-Andrew Pollo-Guilbert
+ */
+public interface IChartViewer {
+
+    /**
+     * List of standard colors
+     */
+    public static final List<Color> COLORS = checkNotNull(ImmutableList.of(
+            new Color(Display.getDefault(), 72, 120, 207),
+            new Color(Display.getDefault(), 106, 204, 101),
+            new Color(Display.getDefault(), 214, 95, 95),
+            new Color(Display.getDefault(), 180, 124, 199),
+            new Color(Display.getDefault(), 196, 173, 102),
+            new Color(Display.getDefault(), 119, 190, 219)));
+
+    /**
+     * Dispose the viewer widget.
+     */
+    void dispose();
+
+    /**
+     * Factory method to create a chart.
+     *
+     * @param parent
+     *            Parent composite
+     * @param data
+     *            Configured data series for the chart
+     * @param model
+     *            Chart model to use
+     * @param title
+     *            Title of the chart
+     * @return The chart object
+     */
+    static @Nullable IChartViewer createChart(Composite parent, ChartData data, ChartModel model) {
+        switch (model.getChartType()) {
+        case BAR_CHART:
+            /**
+             * TODO
+             */
+        case SCATTER_CHART:
+            /**
+             * TODO
+             */
+        case PIE_CHART:
+            /**
+             * TODO
+             */
+        default:
+            return null;
+        }
+    }
+
+}
